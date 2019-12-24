081f8cc1d203890ad80034a0170527748b5d2260
Pavel <tech@hozberg.com>
2019-12-24 05:02:31 +0300

    Верхняя корзина

---
+++ local/templates/main/components/bitrix/sale.basket.basket.line/top_basket/top_template.php
@@ -0,0 +1,20 @@
+<? if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
+    die();
+}
+/**
+ * @global array $arParams
+ * @global CUser $USER
+ * @global CMain $APPLICATION
+ * @global string $cartId
+ */
+?>
+<a class="menu__link" href="#" role="button"
+   data-toggle="dropdown" aria-haspopup="true"
+   aria-expanded="false">
+    <svg class="icon icon-basket menu__icon">
+        <use xlink:href="#basket"></use>
+    </svg>
+    <? if ((int)$arResult['NUM_PRODUCTS'] > 0): ?>
+        <div class="menu__basket-count"><?= $arResult['NUM_PRODUCTS'] ?></div>
+    <? endif; ?>
+</a>
