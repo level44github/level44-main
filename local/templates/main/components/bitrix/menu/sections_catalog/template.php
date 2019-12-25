b6a7d085b0811de2d3339e9d010b5ec2a322f22c
Pavel <tech@hozberg.com>
2019-12-25 05:48:05 +0300

    Список раздела

---
+++ local/templates/main/components/bitrix/menu/sections_catalog/template.php
@@ -0,0 +1,13 @@
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
+    die();
+} ?>
+<? if (!empty($arResult)): ?>
+    <div class="catalog__nav js-catalog__nav">
+        <? foreach ($arResult as $item): ?>
+            <div class="catalog__nav-item">
+                <a class="catalog__nav-link <?= $item["SELECTED"] ? "active" : "" ?>"
+                   href="<?= $item["LINK"] ?>"><?= $item["TEXT"] ?></a>
+            </div>
+        <? endforeach; ?>
+    </div>
+<? endif; ?>
