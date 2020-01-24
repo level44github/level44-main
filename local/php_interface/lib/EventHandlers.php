34825b89cace67f5050aef2f8fd394c0e7b40ffa
Pavel <tech@hozberg.com>
2020-01-25 00:49:07 +0300

    Мелкие доработки

--- local/php_interface/lib/EventHandlers.php
+++ local/php_interface/lib/EventHandlers.php
@@ -3,10 +3,32 @@
 namespace Level44;
 
 
+use Bitrix\Main\EventManager;
+
 class EventHandlers
 {
+    /** @var $instance  EventManager */
+    private static $instance = null;
+
     public static function register()
     {
-        return true;
+        if (!self::$instance) {
+            self::$instance = EventManager::getInstance();
+        }
+    }
+
+    private static function addEventHandler($moduleId, $eventType)
+    {
+        if (!$moduleId || !$eventType || !self::$instance) {
+            return false;
+        }
+        return self::$instance->addEventHandler(
+            $moduleId,
+            $eventType,
+            [
+                self::class,
+                $eventType . "Handler"
+            ]
+        );
     }
 }
