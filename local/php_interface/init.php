73dd345da3d5d3cfe0cb0abc9a5269cfe05116f7
Pavel <tech@hozberg.com>
2020-02-02 13:28:13 +0300

    Авторейсайз для фото товаров

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -10,78 +10,54 @@
 \Level44\Base::customRegistry();
 \Level44\EventHandlers::register();
 
-AddEventHandler("iblock", "OnAfterIBlockElementAdd", Array("MorePhotoResizeClass", "OnAfterIBlockElement"));
-AddEventHandler("iblock", "OnAfterIBlockElementUpdate", Array("MorePhotoResizeClass", "OnAfterIBlockElement"));
+// События которые срабатывают при создании или изменении элемента инфоблока
+AddEventHandler("iblock", "OnAfterIBlockElementAdd", "ResizeUploadedPhoto");
+AddEventHandler("iblock", "OnAfterIBlockElementUpdate", "ResizeUploadedPhoto");
 
-class MorePhotoResizeClass
-{
-    function Clear()
-    {
-        $_WFILE = glob($_SERVER['DOCUMENT_ROOT']."/upload/tmp/*");
-        foreach($_WFILE as $_file)
-            unlink($_file);
-        return true;
-    }
-
-    function OnAfterIBlockElement(&$arFields)
-    {
-        global $APPLICATION, $USER;
-
-        $PROPERTY_CODE = "MORE_PHOTO"; // код свойства с которым работаем
-
-        $imageMaxWidth = 900; // Максимальная ширина уменьшенной картинки
-        $imageMaxHeight = 1600; // Максимальная высота уменьшенной картинки
-
-        // Находим свойство
-        $dbRes = CIBlockElement::GetProperty($arFields["IBLOCK_ID"], $arFields["ID"], "sort", "asc", array("CODE" => $PROPERTY_CODE));
-        while ($arMorePhoto = $dbRes->GetNext(true, false))
-        {
-            if ($arMorePhoto["PROPERTY_TYPE"] == "F" && $arMorePhoto["MULTIPLE"] == "Y")
-            {
-                // находим подробные сведения о файле
-                $arFile = CFile::GetFileArray($arMorePhoto["VALUE"]);
-
-                // проверяем, что файл является картинкой
-                if (!CFile::IsImage($arFile["FILE_NAME"]))
-                {
-                    continue;
+function ResizeUploadedPhoto($arFields) {
+    global $APPLICATION;
+    CModule::IncludeModule('iblock');
+    $IBLOCK_ID = 2; // ID инфоблока свойство которых нуждается в масштабировании
+    $PROPERTY_CODE = "MORE_PHOTO";  // код свойства
+    $imageMaxWidth = 900; // Максимальная ширина картинки
+    $imageMaxHeight = 1600; // Максимальная высота картинки
+    // для начала убедимся, что изменяется элемент нужного нам инфоблока
+    if($arFields["IBLOCK_ID"] == $IBLOCK_ID) {
+        $VALUES = $VALUES_OLD = array();
+        //Получаем свойство значение сво-ва $PROPERTY_CODE
+        $res = CIBlockElement::GetProperty($arFields["IBLOCK_ID"], $arFields["ID"], "sort", "asc", array("CODE" => $PROPERTY_CODE));
+        while ($ob = $res->GetNext()) {
+            $file_path = CFile::GetPath($ob['VALUE']); // Получаем путь к файлу
+            if($file_path) {
+                $imsize = getimagesize($_SERVER["DOCUMENT_ROOT"].$file_path); //Узнаём размер файла
+                // Если размер больше установленного максимума
+                if($imsize[0] > $imageMaxWidth or $imsize[1] > $imageMaxHeight) {
+                    // Уменьшаем размер картинки
+                    $file = CFile::ResizeImageGet($ob['VALUE'], array(
+                        'width'=>$imageMaxWidth,
+                        'height'=>$imageMaxHeight
+                    ), BX_RESIZE_IMAGE_PROPORTIONAL, true);
+                    // добавляем в массив VALUES новую уменьшенную картинку
+                    $VALUES[] = CFile::MakeFileArray($_SERVER["DOCUMENT_ROOT"].$file["src"]);
+                } else {
+                    // добавляем в массив VALUES старую картинку
+                    $VALUES[] = CFile::MakeFileArray($_SERVER["DOCUMENT_ROOT"].$file_path);
                 }
-
-                // Если размер больше допустимого
-                if ($arFile["WIDTH"] > $imageMaxWidth || $arFile["HEIGHT"] > $imageMaxHeight)
-                {
-                    // Временная картинка
-                    $tmpFilePath = $_SERVER['DOCUMENT_ROOT']."/upload/tmp/".$arFile["FILE_NAME"];
-
-                    // Уменьшаем картинку
-                    $resizeRez = CFile::ResizeImageFile( // уменьшение картинки для превью
-                        $source = $_SERVER['DOCUMENT_ROOT'].$arFile["SRC"],
-                        $dest = $tmpFilePath,
-                        array(
-                            'width' => $imageMaxWidth,
-                            'height' => $imageMaxHeight,
-                        ),
-                        $resizeType = BX_RESIZE_IMAGE_PROPORTIONAL, // метод ресайза
-                        $jpgQuality = 95
-
-               );
-
-               // Записываем изменение в свойство
-               if ($resizeRez)
-               {
-                   $arNewFile = CFile::MakeFileArray($tmpFilePath);
-
-                   CIBlockElement::SetPropertyValueCode($arFields["ID"], $PROPERTY_CODE,
-                       array($arMorePhoto["PROPERTY_VALUE_ID"] => array(
-                           "VALUE" => $arNewFile,
-                           "DESCRIPTION"=> $arMorePhoto["DESCRIPTION"],
-                       ))
-                   );
-                   // Стираем временные файлы
-                   MorePhotoResizeClass::Clear();
-               }
-            }
+                // Собираем в массив ID старых файлов для их удаления (чтобы не занимали место)
+                $VALUES_OLD[] = $ob['VALUE'];
             }
         }
+        // Если в массиве есть информация о новых файлах
+        if(count($VALUES) > 0) {
+            $PROPERTY_VALUE = $VALUES;  // значение свойства
+            // Установим новое значение для данного свойства данного элемента
+            CIBlockElement::SetPropertyValuesEx($arFields["ID"], $arFields["IBLOCK_ID"], array($PROPERTY_CODE => $PROPERTY_VALUE));
+            // Удаляем старые большие изображения
+            foreach ($VALUES_OLD as $key=>$val) {
+                CFile::Delete($val);
+            }
+        }
+        unset($VALUES);
+        unset($VALUES_OLD);
     }
 }
