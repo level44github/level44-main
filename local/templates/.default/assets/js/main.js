bfe5dfe958028816794f00762bc631ab6f1abf15
Ksenia Balashova <utyatka@gmail.com>
2020-03-24 12:40:43 +0100

    iphone 100vh

--- local/templates/.default/assets/js/main.js
+++ local/templates/.default/assets/js/main.js
@@ -4,4 +4,9 @@ $(function () {
             event.preventDefault();
         }
     })
+
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
 })
+
