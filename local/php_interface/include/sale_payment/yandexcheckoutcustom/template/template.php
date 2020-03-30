701fa52200aa86dc10bb4f3fb86af678f7c89607
Ksenia Balashova <utyatka@gmail.com>
2020-03-30 09:54:17 +0200

    btn fix

--- local/php_interface/include/sale_payment/yandexcheckoutcustom/template/template.php
+++ local/php_interface/include/sale_payment/yandexcheckoutcustom/template/template.php
@@ -8,7 +8,7 @@ $sum = round($params['SUM'], 2);
 <div class="mb-4 ya_cashier" >
 	<div class="d-flex align-items-center mb-3">
 		<div class="col-auto pl-0">
-			<a class="btn btn-dark thank-order__btn btn-primary js-ya_cashier_link" href="<?=$params['URL'];?>"><?=Loc::getMessage('SALE_HANDLERS_PAY_SYSTEM_YANDEX_CHECKOUT_BUTTON_PAID')." ".SaleFormatCurrency($sum, $params['CURRENCY']);?></a>
+			<a class="btn btn-dark thank-order__btn btn__fix-width btn-primary js-ya_cashier_link" href="<?=$params['URL'];?>"><?=Loc::getMessage('SALE_HANDLERS_PAY_SYSTEM_YANDEX_CHECKOUT_BUTTON_PAID')." ".SaleFormatCurrency($sum, $params['CURRENCY']);?></a>
 		</div>
 	</div>
 </div>
