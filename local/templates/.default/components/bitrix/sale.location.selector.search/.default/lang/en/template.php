e6f36234a818abc08178a76437a23f6d74e45fe3
Pavel <pavel@hozberg.com>
2020-09-03 23:36:10 +0300

    СДЭК для заказов в Беларусь и Казахстан

--- local/templates/.default/components/bitrix/sale.location.selector.search/.default/lang/en/template.php
+++ local/templates/.default/components/bitrix/sale.location.selector.search/.default/lang/en/template.php
@@ -4,4 +4,5 @@ $MESS["SALE_SLS_ERROR_OCCURED"] = "There was an error";
 $MESS["SALE_SLS_CLEAR_SELECTION"] = "Clear selection";
 $MESS["SALE_SLS_EDIT"] = "Edit";
 $MESS["SALE_SLS_INPUT_SOME"] = "Enter name";
+$MESS["SNG_PLACEHOLDER"] = "Enter city in Russia, Belarus or Kazakhstan";
 ?>
