4de8762b0650d77492bca82f4bdd1211fc845524
Pavel <hozberg97@mail.ru>
2022-05-29 16:30:08 +0300

    Вам может понравиться

--- local/php_interface/lib/Content.php
+++ local/php_interface/lib/Content.php
@@ -13,6 +13,7 @@ use Bitrix\Main\ArgumentException;
 use Bitrix\Main\LoaderException;
 use Bitrix\Main\ObjectPropertyException;
 use Bitrix\Main\SystemException;
+use Bitrix\Main\Type\Collection;
 
 /**
  * Class Content
@@ -55,5 +56,102 @@ class Content
         }
 
         return $arResult;
+    }
+
+    /**
+     * @param int $productId
+     * @return array
+     */
+    public static function recommendProductsOrdered(int $productId)
+    {
+        //Получаем привязки свойства "Другие товары из комплекта"
+        $result = \CIBlockElement::GetProperty(Base::CATALOG_IBLOCK_ID, $productId, [], ["CODE" => "OTHER_KIT_ITEMS"]);
+
+        $resultList = [];
+
+        while ($row = $result->GetNext()) {
+            //Исключаем текущий товар
+            if ((int)$row["ID"] !== (int)$productId) {
+                $resultList[] = (int)$row["VALUE"];
+            }
+        }
+
+        //Получаем все разделы текущего товара
+        $result = \CIBlockElement::GetElementGroups($productId, true, ["ID", "SORT"]);
+
+        $sections = [];
+        while ($row = $result->GetNext()) {
+            $sections[$row["ID"]] = $row;
+        }
+
+        //Сортируем разделы в соответствии с индексом сортировки
+        Collection::sortByColumn($sections, ["SORT" => SORT_ASC], "", null, true);
+
+        //Получаем все товары из данных разделов
+        $result = \CIBlockElement::GetList(
+            [],
+            [
+                "IBLOCK_ID"  => Base::CATALOG_IBLOCK_ID,
+                "SECTION_ID" => array_keys($sections),
+                "=AVAILABLE" => "Y",
+                "ACTIVE"     => "Y",
+            ],
+            false,
+            false,
+            [
+                "ID",
+                "IBLOCK_ID",
+                "SORT",
+            ]);
+
+        $productsForSections = [];
+        while ($row = $result->GetNext()) {
+            //Исключаем текущий товар
+            if ((int)$row["ID"] !== (int)$productId) {
+                $productsForSections[$row["ID"]] = $row;
+            }
+        }
+
+        //Получаем все разделы для товаров из этих разделов
+        //(Что бы разместить товар в разделе с наименьшим индексом сортировки)
+        $result = \CIBlockElement::GetElementGroups(array_keys($productsForSections), true);
+
+        while ($row = $result->GetNext()) {
+            if (array_key_exists($row["ID"], $sections)) {
+                //Раскидываем товары по разделам
+                $sections[$row["ID"]]["PRODUCTS"][] = [
+                    "ID"   => $row["IBLOCK_ELEMENT_ID"],
+                    "SORT" => $productsForSections[$row["IBLOCK_ELEMENT_ID"]]["SORT"],
+                ];
+            }
+        }
+
+        //Внутри каждого раздела, сортируем товары в соответствии с сортировкой в каталоге
+        foreach ($sections as &$section) {
+            Collection::sortByColumn($section["PRODUCTS"], ['SORT' => SORT_ASC, 'ID' => SORT_DESC]);
+        }
+        unset($section);
+
+
+        do {
+            $commonCount = 0;
+
+            foreach ($sections as &$section) {
+                $product = array_shift($section["PRODUCTS"]);
+
+                if ($product["ID"] && !in_array($product["ID"], $resultList)) {
+                    $resultList[] = (int)$product["ID"];
+                }
+
+                $commonCount += count($section["PRODUCTS"]);
+            }
+            unset($section);
+
+        } while ($commonCount > 0);
+
+        //Удаляем пустые значения
+        $resultList = array_values(array_filter($resultList));
+
+        return $resultList;
     }
 }
