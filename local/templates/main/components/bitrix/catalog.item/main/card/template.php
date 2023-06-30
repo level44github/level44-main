32c00031f893aab60b192faa3bb9bf04d7dab3e6
Vadim <vadic.m@yandex.ru>
2023-06-30 12:31:29 +0300

    fix

--- local/templates/main/components/bitrix/catalog.item/main/card/template.php
+++ local/templates/main/components/bitrix/catalog.item/main/card/template.php
@@ -27,11 +27,11 @@ use \Bitrix\Main\Localization\Loc;
 
 <div class="catalog__scroll">
     <a class="catalog__item-image" href="<?= $item['DETAIL_PAGE_URL'] ?>">
+    <div class="catalog__item-wrapper">
         <? foreach ($item["PREVIEW_IMAGES"] as $previewImage): ?>
-            <div class="catalog__item-wrapper">
-                <img class="img-fluid" src="<?= $previewImage ?>" alt="<?= $item['NAME'] ?>">
-            </div>
+            <img class="img-fluid" src="<?= $previewImage ?>" alt="<?= $item['NAME'] ?>">
         <? endforeach; ?>
+    </div>
     </a>
 </div>
 <div class="catalog__item-footer">
