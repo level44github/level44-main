a8f6564771d97701aef9cbbb00141958a09a80d3
Vadim <vadic.M@yandex.ru>
2023-07-04 12:02:04 +0300

    Feat/26.06.23 (#132)
    * video loop
    
    * add arrow
    
    * add scroll

--- local/templates/main/components/bitrix/catalog.item/main/card/template.php
+++ local/templates/main/components/bitrix/catalog.item/main/card/template.php
@@ -25,12 +25,13 @@ use \Bitrix\Main\Localization\Loc;
  */
 ?>
 
-
-<? foreach ($item["PREVIEW_IMAGES"] as $previewImage): ?>
+<div class="catalog__scroll">
     <a class="catalog__item-image" href="<?= $item['DETAIL_PAGE_URL'] ?>">
-        <img class="img-fluid" src="<?= $previewImage ?>" alt="<?= $item['NAME'] ?>">
+        <? foreach ($item["PREVIEW_IMAGES"] as $previewImage): ?>
+            <img class="img-fluid" src="<?= $previewImage ?>" alt="<?= $item['NAME'] ?>">
+        <? endforeach; ?>
     </a>
-<? endforeach; ?>
+</div>
 <div class="catalog__item-footer">
     <a class="catalog__item-title" href="<?= $item['DETAIL_PAGE_URL'] ?>"><?= $item['NAME'] ?></a>
     <div class="<?= $price["oldPrice"] ? "product__final-price" : "" ?>">
