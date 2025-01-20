ffc4d473b089ae5449ba109fc93b921b5d2a9b44
Pavel Hozberg <pavel.hozberg@gmail.com>
2025-01-20 16:22:54 +0300

    Menu

--- local/templates/main/components/bitrix/menu/sections_catalog/result_modifier.php
+++ local/templates/main/components/bitrix/menu/sections_catalog/result_modifier.php
@@ -2,7 +2,22 @@
     die();
 }
 
+/** @var array $arResult */
+
 global $APPLICATION;
+function setActive(array &$items): void
+{
+    global $APPLICATION;
+
+    foreach ($items as &$item) {
+        $children = $item["PARAMS"]["CHILDREN"];
+
+        if (is_array($children)) {
+            $item["SELECTED"] = array_filter($children, fn($child) => $child[1] === $APPLICATION->GetCurDir());
+        }
+    }
+    unset($item);
+}
 
 foreach ($arResult as &$arItem) {
     if ($arItem["SELECTED"]) {
@@ -12,3 +27,7 @@ foreach ($arResult as &$arItem) {
     }
 }
 unset($arItem);
+
+if (empty(array_filter($arResult, fn($item) => $item["SELECTED"]))) {
+    setActive($arResult);
+}
