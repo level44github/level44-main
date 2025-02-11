de6dc414c56407c129cdee798ba616ed43726c92
Pavel Hozberg <pavel.hozberg@gmail.com>
2025-02-11 14:38:48 +0300

    Интеграция доставок: Автодополнение адресов (СНГ)

--- local/templates/.default/components/bitrix/sale.order.ajax/main/delivery.php
+++ local/templates/.default/components/bitrix/sale.order.ajax/main/delivery.php
@@ -2,6 +2,7 @@
     die();
 }
 use Bitrix\Main\Localization\Loc;
+use Bitrix\Main\Web\Json;
 
 ?>
 
@@ -44,38 +45,15 @@ use Bitrix\Main\Localization\Loc;
                     <div class="collapse <?= $delivery["CHECKED"] ? "show" : "" ?>" id="delivery<?= $key ?>"
                          data-parent="#delivery">
                         <div class="option__body">
-                            <? if (!empty($arResult["ORDER_PROP_ADDRESS"]) && $arResult["ORDER_PROP_ADDRESS"]["TYPE"] === "STRING"):
-                                $required = $arResult["ORDER_PROP_ADDRESS"]["REQUIRED"];
-                                ?>
-                                    <div class="form-group">
-                                    <label for="form-delivery<?= $key ?>-address"><?= $arResult["ORDER_PROP_ADDRESS"]["NAME"] ?></label>
-                                        <input class="form-control js-form__control <?= $required ? "is-required" : "" ?>"
-                                               type="text"
-                                               id="form-delivery<?= $key ?>-address"
-                                               maxlength="250"
-                                               data-delivery="<?=$delivery["ID"]?>"
-                                               size="<?= $arResult["ORDER_PROP_ADDRESS"]["SIZE1"] ?>"
-                                               name="<?= $arResult["ORDER_PROP_ADDRESS"]["FIELD_NAME"] ?>-fake"
-                                               value="<?= $arResult["ORDER_PROP_ADDRESS"]["VALUE"] ?>"
-                                               data-prop="<?= $arResult["ORDER_PROP_ADDRESS"]["CODE"] ?>"
-                                               placeholder="<?= $arResult["ORDER_PROP_ADDRESS"]["DESCRIPTION"] ?>"
-                                        >
-                                        <div class="invalid-feedback">
-                                            <?= Loc::getMessage($arResult["ORDER_PROP_ADDRESS"]["ERROR_MES_TYPE"], [
-                                                "#FIELD#" => $arResult["ORDER_PROP_ADDRESS"]["NAME"]
-                                            ]) ?>
-                                        </div>
-                                    </div>
-                            <? endif; ?>
                             <? if (in_array($delivery["ID"], CDeliverySDEK::getDeliveryId('pickup'))): ?>
-                                <? if (!empty($arResult["ORDER_PROP_ADDRESS_SDEK"]) && $arResult["ORDER_PROP_ADDRESS_SDEK"]["TYPE"] === "STRING"):?>
+                                <? if (!empty($arResult["ORDER_PROP_ADDRESS_SDEK"]) && $arResult["ORDER_PROP_ADDRESS_SDEK"]["TYPE"] === "STRING"): ?>
                                     <div class="form-group">
                                         <input class="form-control js-form__control"
                                                type="text"
                                                style="display: none"
                                                id="form-delivery<?= $key ?>-address"
                                                maxlength="250"
-                                               data-delivery="<?=$delivery["ID"]?>"
+                                               data-delivery="<?= $delivery["ID"] ?>"
                                                size="<?= $arResult["ORDER_PROP_ADDRESS_SDEK"]["SIZE1"] ?>"
                                                name="<?= $arResult["ORDER_PROP_ADDRESS_SDEK"]["FIELD_NAME"] ?>"
                                                value="<?= $arResult["ORDER_PROP_ADDRESS_SDEK"]["VALUE"] ?>"
@@ -92,6 +70,42 @@ use Bitrix\Main\Localization\Loc;
                                         </div>
                                     </div>
                                 </div>
+                            <? else: ?>
+                                <? if (!empty($arResult["ORDER_PROP_ADDRESS"]) && $arResult["ORDER_PROP_ADDRESS"]["TYPE"] === "STRING"):
+                                    $required = $arResult["ORDER_PROP_ADDRESS"]["REQUIRED"];
+                                    ?>
+                                    <div class="form-group">
+                                        <label for="form-delivery<?= $key ?>-address"><?= $arResult["ORDER_PROP_ADDRESS"]["NAME"] ?></label>
+                                        <input class="form-control js-form__control js-address-field <?= $required ? "is-required" : "" ?>"
+                                               type="text"
+                                               id="form-delivery<?= $key ?>-address"
+                                               maxlength="250"
+                                               data-delivery="<?= $delivery["ID"] ?>"
+                                               size="<?= $arResult["ORDER_PROP_ADDRESS"]["SIZE1"] ?>"
+                                               name="<?= $arResult["ORDER_PROP_ADDRESS"]["FIELD_NAME"] ?>-fake"
+                                               value="<?= $arResult["ORDER_PROP_ADDRESS"]["VALUE"] ?>"
+                                               data-prop="<?= $arResult["ORDER_PROP_ADDRESS"]["CODE"] ?>"
+                                               placeholder="<?= $arResult["ORDER_PROP_ADDRESS"]["DESCRIPTION"] ?>"
+                                        >
+                                        <div class="invalid-feedback">
+                                            <?= Loc::getMessage($arResult["ORDER_PROP_ADDRESS"]["ERROR_MES_TYPE"], [
+                                                "#FIELD#" => $arResult["ORDER_PROP_ADDRESS"]["NAME"]
+                                            ]) ?>
+                                        </div>
+
+                                        <script>
+                                            BX.saleOrderAjax.address.previousValue = <?=Json::encode($arResult["ORDER_PROP_ADDRESS"]["VALUE"])?>;
+                                            BX.saleOrderAjax.address.errors = {
+                                                EMPTY: <?=Json::encode(Loc::getMessage($arResult["ORDER_PROP_ADDRESS"]["ERROR_MES_TYPE"], [
+                                                    "#FIELD#" => $arResult["ORDER_PROP_ADDRESS"]["NAME"]
+                                                ]))?>,
+                                                NOT_SELECTED: <?=Json::encode(Loc::getMessage('ADDRESS_NOT_SELECTED_ERROR_MES'))?>,
+                                                MISSED_HOUSE: <?=Json::encode(Loc::getMessage('ADDRESS_MISSED_HOUSE_ERROR_MES'))?>,
+                                                MISSED_FLAT: <?=Json::encode(Loc::getMessage('ADDRESS_MISSED_FLAT_ERROR_MES'))?>,
+                                            }
+                                        </script>
+                                    </div>
+                                <? endif; ?>
                             <? endif; ?>
                         </div>
                     </div>
