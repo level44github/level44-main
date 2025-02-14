cdbb12a2241c3126506791274afc261c35d16d49
SerafimNikalayeu <serafim.nikalayeu@humandev.org>
2025-02-14 12:46:13 -0800

    fix: bottom text

--- local/templates/main/lang/en/header.php
+++ local/templates/main/lang/en/header.php
@@ -1,4 +1,8 @@
 <?
 $MESS["HEADER_SEARCH_ON_SITE"] = "Find on the site";
 $MESS["HEADER_GO_CATALOG"] = "New";
+$MESS["HEADER_BANNER_0_TITLE"] = "<h1>Идеальный гардероб</h1>";
+$MESS["HEADER_BANNER_0_TEXT"] = "<p>Лаконично, но ярко: стильные инвестиции</p>";
+$MESS["HEADER_BANNER_1_TITLE"] = "<h1>Любовь в большом городе</h1>";
+$MESS["HEADER_BANNER_1_TEXT"] = "<a href='#'>Смотреть коллекцию</a>";
 ?>
--- local/templates/main/lang/ru/header.php
+++ local/templates/main/lang/ru/header.php
@@ -1,8 +1,8 @@
 <?
 $MESS["HEADER_SEARCH_ON_SITE"] = "Найти на сайте";
 $MESS["HEADER_GO_CATALOG"] = "Новинки";
-$MESS["HEADER_BANNER_1_TITLE"] = "<h1>Идеальный гардероб</h1>";
-$MESS["HEADER_BANNER_1_TEXT"] = "<p>Лаконично, но ярко: стильные инвестиции</p>";
-$MESS["HEADER_BANNER_2_TITLE"] = "<h1>Любовь в большом городе</h1>";
-$MESS["HEADER_BANNER_2_TEXT"] = "<a href='#'>Смотреть коллекцию</a>";
+$MESS["HEADER_BANNER_0_TITLE"] = "<h1>Идеальный гардероб</h1>";
+$MESS["HEADER_BANNER_0_TEXT"] = "<p>Лаконично, но ярко: стильные инвестиции</p>";
+$MESS["HEADER_BANNER_1_TITLE"] = "<h1>Любовь в большом городе</h1>";
+$MESS["HEADER_BANNER_1_TEXT"] = "<a href='#'>Смотреть коллекцию</a>";
 ?>
