d1aaae459a99d675caf5d891436933b37cf6701c
SerafimNikalayeu <serafim.nikalayeu@humandev.org>
2025-02-15 01:40:43 -0800

    fix: fonts & positioning

--- local/templates/main/header.php
+++ local/templates/main/header.php
@@ -92,10 +92,11 @@ $searchQuery = (string) \Bitrix\Main\Context::getCurrent()
                                         <img src="<?= $mobileSlide['splitSrc'] ?>" alt="Banner part 1">
                                     </div>
                                 <?php endif; ?>
-                                <div class="swiper-pagination-wrapper">
-                                    <div class="swiper-pagination-wrapper-block">
+                                <div class="swiper-about-wrapper">
+                                    <div class="swiper-about-wrapper-block">
                                         <?=Loc::getMessage("HEADER_BANNER_{$index}_TITLE")?>
                                         <?=Loc::getMessage("HEADER_BANNER_{$index}_TEXT")?>
+                                        <?=Loc::getMessage("HEADER_BANNER_{$index}_LINK")?>
                                     </div>
                                 </div>
                             </div>
