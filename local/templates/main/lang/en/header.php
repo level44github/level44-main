9382c2cac12ca9e827a54118ce294d6c53beabd4
SerafimNikalayeu <serafim.nikalayeu@humandev.org>
2025-02-17 06:28:40 -0800

    fix: about block for video
    del: ru/en header fields
    
    fix: arrows margin
    
    fix: root values
    
    fix: video styles

--- local/templates/main/lang/en/header.php
+++ local/templates/main/lang/en/header.php
@@ -1,10 +1,4 @@
 <?
 $MESS["HEADER_SEARCH_ON_SITE"] = "Find on the site";
 $MESS["HEADER_GO_CATALOG"] = "New";
-$MESS["HEADER_BANNER_0_TITLE"] = "<h1>Идеальный гардероб</h1>";
-$MESS["HEADER_BANNER_0_TEXT"] = "<p>Лаконично, но ярко: стильные инвестиции</p>";
-$MESS["HEADER_BANNER_0_LINK"] = null;
-$MESS["HEADER_BANNER_1_TITLE"] = "<h1>Любовь в большом городе</h1>";
-$MESS["HEADER_BANNER_1_TEXT"] = null;
-$MESS["HEADER_BANNER_1_LINK"] = "<a href='#'>Смотреть коллекцию</a>";
 ?>
