9b5610f5a66a4bbf23a88275139769448a8cf862
hpav <30503521+hpav@users.noreply.github.com>
2025-03-04 19:47:49 +0300

    Header banner backend (#190)
    * Управление баннерами
    
    * Управление баннерами
    
    * Управление баннерами

--- local/php_interface/lib/Level44/Base.php
+++ local/php_interface/lib/Level44/Base.php
@@ -3,10 +3,14 @@
 namespace Level44;
 
 use Bitrix\Catalog\PriceTable;
+use Bitrix\Iblock\ElementTable;
 use Bitrix\Iblock\PropertyTable;
+use Bitrix\Main\ArgumentException;
 use Bitrix\Main\Context;
+use Bitrix\Main\ObjectPropertyException;
 use \Bitrix\Main\Page\Asset;
 use Bitrix\Main\Loader;
+use Bitrix\Main\SystemException;
 use Bitrix\Sale\Location\LocationTable;
 use Bitrix\Sale\Order;
 use Bitrix\Sale\Registry;
@@ -21,6 +25,7 @@ class Base
 {
     const OFFERS_IBLOCK_ID = 3;
     const CATALOG_IBLOCK_ID = 2;
+    const BANNER_SLIDES_IBLOCK_ID = 5;
     const CATALOG_VAT_ID = 3;
     const COLOR_HL_TBL_NAME = "eshop_color_reference";
     const IMAGES_ORIGINAL_HL_TBL_NAME = "images_original";
@@ -107,28 +112,6 @@ class Base
             }
         } catch (\Exception $e) {
         }
-    }
-
-    public static function getMainBanner($mobile = false): array
-    {
-        $imageUrl = "";
-        if (Loader::includeModule("germen.settings")) {
-            if ($mobile) {
-                $imageId = (int)Settings::get("MAIN_BANNER_MOBILE");
-            } else {
-                $imageId = (int)Settings::get("MAIN_BANNER");
-            }
-            $imageUrl = (string)\CFile::GetFileArray($imageId)["SRC"];
-        }
-
-        if (empty($imageUrl)) {
-            $imageUrl = $mobile ? self::getAssetsPath() . "/img/home-mobile.jpg" : self::getAssetsPath() . "/img/home.jpg";
-        }
-
-        return [
-            'src'     => $imageUrl,
-            'isVideo' => preg_match('/\.(mpg|avi|wmv|mpeg|mpe|flv|mp4)$/i', $imageUrl),
-        ];
     }
 
     public static function setColorOffers(&$linkedElements, &$currentElement = [])
