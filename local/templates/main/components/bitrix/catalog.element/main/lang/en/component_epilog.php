e55a4576f27bd2536e7098f306a4acb7d6a3dec9
Pavel Hozberg <pavel.hozberg@gmail.com>
2025-05-21 15:08:15 +0300

    Редизайн: Карточка товара (fix модальных окон)

---
+++ local/templates/main/components/bitrix/catalog.element/main/lang/en/component_epilog.php
@@ -0,0 +1,15 @@
+<?
+$MESS["TITLE_POPUP_SUBSCRIBED"] = "Availability notification";
+$MESS["CPST_SUBSCRIBE_BUTTON_CLOSE"] = "Close";
+$MESS["DESC_POPUP_SUBSCRIBED"] = "Please, leave your email and we will notify you as soon as this item available again.";
+$MESS["PHONE_POPUP_SUBSCRIBED"] = "Phone";
+$MESS["BTN_SEND_POPUP_SUBSCRIBED"] = "Subscribe";
+$MESS["SUCCESS_POPUP_SUBSCRIBED"] = "You have successfully subscribed";
+$MESS["SIZE_TABLE"] = "Size table";
+$MESS["SIZE_TABLE_SIZE"] = "Size";
+$MESS["SIZE_TABLE_FOOT_LENGTH"] = "Foot length (cm)";
+$MESS["CHEST_CIRCUMFERENCE"] = "Chest girth (cm)";
+$MESS["HOIST_GIRTH"] = "Waist circumference";
+$MESS["HIP_GIRTH"] = "Hip girth";
+
+?>
---
+++ local/templates/main/components/bitrix/catalog.element/main/lang/ru/component_epilog.php
@@ -0,0 +1,15 @@
+<?
+$MESS["TITLE_POPUP_SUBSCRIBED"] = "Подписаться на товар";
+$MESS["CPST_SUBSCRIBE_BUTTON_CLOSE"] = "Закрыть";
+$MESS["DESC_POPUP_SUBSCRIBED"] = "Оставьте адрес электронной почты, и мы напишем вам, когда товар появится в наличии.";
+$MESS["PHONE_POPUP_SUBSCRIBED"] = "Телефон";
+$MESS["BTN_SEND_POPUP_SUBSCRIBED"] = "Подписаться";
+$MESS["SUCCESS_POPUP_SUBSCRIBED"] = "Вы успешно подписались";
+$MESS["SIZE_TABLE"] = "Таблица размеров";
+$MESS["SIZE_TABLE_SIZE"] = "Размер";
+$MESS["SIZE_TABLE_FOOT_LENGTH"] = "Длина стопы (см)";
+$MESS["CHEST_CIRCUMFERENCE"] = "Обхват груди (см)";
+$MESS["HOIST_GIRTH"] = "Обхват талии";
+$MESS["HIP_GIRTH"] = "Обхват бедер";
+
+?>
