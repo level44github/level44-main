e2a565c0d6863533c7c4818f5bfac34da1b517a2
Kopcap <kopcapx@mail.ru>
2025-09-22 17:59:11 +0500

    баннер фикс

--- local/components/system/empty/templates/main_banner/template.php
+++ local/components/system/empty/templates/main_banner/template.php
@@ -9,64 +9,66 @@
                 $countSlides=0;
                 foreach ($arResult['SLIDES'] as $i=>$slide){
 
+                    $oldtype=$curenttype;
+                    $curenttype=$slide['banner_type'];
 
-                    if (($i%2 == 0) or ($oldtype=='100%') or ($slide['banner_type']=='100%')){
-                        $oldtype=$curenttype;
-                        $curenttype=$slide['banner_type'];
+                    if (($oldtype=='100%') or ($slide['banner_type']=='100%')){
                         $countSlides++;
                         ?>
-                    <div class="embla__slide" style="justify-content: space-between; display: flex">
+                        <div class="embla__slide" style="justify-content: space-between; display: flex">
                     <?}?>
 
-                        <a class="embla__slide-link" style="width:<?=$slide['banner_type']?>; display:block;"
-                            <? if (!empty($slide['link']['address'])): ?>
-                                href="<?= $slide['link']['address'] ?>"
-                            <? endif; ?>
-                        >
-                            <div class="embla__slide-content" <?if ($curenttype=='100%'){?>style="height: 100svh;"<?}?>>
-                                <? if (!empty($desktopSingle = $slide['files']['desktop']['single'])): ?>
-                                    <? if ($desktopSingle['isVideo']): ?>
-                                        <video class="banner-video desktop" autoplay muted loop playsinline>
-                                            <source src="<?= $desktopSingle['src'] ?>" type="video/mp4">
-                                            Your browser does not support the video tag.
-                                        </video>
-                                    <? else: ?>
-                                        <img class="banner desktop" src="<?= $desktopSingle['src'] ?>"
-                                             alt="<?= $slide['title'] ?>">
-                                    <? endif; ?>
-                                <? elseif (!empty($desktopSplit = $slide['files']['desktop']['split'])): ?>
-                                    <div class="embla__slide-double-images desktop">
-                                        <? foreach ($desktopSplit as $key => $part): ?>
-                                            <img src="<?= $part['src'] ?>" alt="<?= $slide['title'] ?>">
-                                        <? endforeach; ?>
-                                    </div>
-                                <? endif; ?>
-
-                                <? if ($slide['files']['mobile']['isVideo']): ?>
-                                    <video class="banner-video mobile" autoplay muted loop playsinline>
-                                        <source src="<?= $slide['files']['mobile']['src'] ?>" type="video/mp4">
+                    <a class="embla__slide-link" style="width:<?=$slide['banner_type']?>; display:block;"
+                        <? if (!empty($slide['link']['address'])): ?>
+                            href="<?= $slide['link']['address'] ?>"
+                        <? endif; ?>
+                    >
+                        <div class="embla__slide-content" <?if ($curenttype=='100%'){?>style="height: 100svh;"<?}?>>
+                            <? if (!empty($desktopSingle = $slide['files']['desktop']['single'])): ?>
+                                <? if ($desktopSingle['isVideo']): ?>
+                                    <video class="banner-video desktop" autoplay muted loop playsinline>
+                                        <source src="<?= $desktopSingle['src'] ?>" type="video/mp4">
                                         Your browser does not support the video tag.
                                     </video>
                                 <? else: ?>
-                                    <img class="banner mobile" src="<?= $slide['files']['mobile']['src'] ?>"
+                                    <img class="banner desktop" src="<?= $desktopSingle['src'] ?>"
                                          alt="<?= $slide['title'] ?>">
                                 <? endif; ?>
-
-                                <div class="slide-info">
-                                    <? if (!empty($slide['text'])): ?>
-                                        <h4 class="slide-uptitle"><?= $slide['text'] ?></h4>
-                                    <? endif; ?>
-                                    <? if (!empty($slide['title'])): ?>
-                                        <h2 class="slide-title"><?= $slide['title'] ?></h2>
-                                    <? endif; ?>
-                                    <? if (!empty($slide['link']['text'])): ?>
-                                        <button class="btn btn-text slide-btn"><?= $slide['link']['text'] ?></button>
-                                    <? endif; ?>
+                            <? elseif (!empty($desktopSplit = $slide['files']['desktop']['split'])): ?>
+                                <div class="embla__slide-double-images desktop">
+                                    <? foreach ($desktopSplit as $key => $part): ?>
+                                        <img src="<?= $part['src'] ?>" alt="<?= $slide['title'] ?>">
+                                    <? endforeach; ?>
                                 </div>
+                            <? endif; ?>
+
+                            <? if ($slide['files']['mobile']['isVideo']): ?>
+                                <video class="banner-video mobile" autoplay muted loop playsinline>
+                                    <source src="<?= $slide['files']['mobile']['src'] ?>" type="video/mp4">
+                                    Your browser does not support the video tag.
+                                </video>
+                            <? else: ?>
+                                <img class="banner mobile" src="<?= $slide['files']['mobile']['src'] ?>"
+                                     alt="<?= $slide['title'] ?>">
+                            <? endif; ?>
+
+                            <div class="slide-info">
+                                <? if (!empty($slide['text'])): ?>
+                                    <h4 class="slide-uptitle"><?= $slide['text'] ?></h4>
+                                <? endif; ?>
+                                <? if (!empty($slide['title'])): ?>
+                                    <h2 class="slide-title"><?= $slide['title'] ?></h2>
+                                <? endif; ?>
+                                <? if (!empty($slide['link']['text'])): ?>
+                                    <button class="btn btn-text slide-btn"><?= $slide['link']['text'] ?></button>
+                                <? endif; ?>
                             </div>
-                        </a>
-                   <? if (($i%2 != 0) or ($slide['banner_type']=='100%') or ($arResult['SLIDES'][$i+1]['banner_type']=='100%')){?>
-                    </div>
+                        </div>
+                    </a>
+                    <?
+
+                    if (($curenttype=='100%') or ($arResult['SLIDES'][$i+1]['banner_type']=='100%') or (($curenttype=='50%') and ($oldtype=='50%'))){?>
+                        </div>
                     <?}?>
                 <? } ?>
 
