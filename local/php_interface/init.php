515eda8a68c0fa569920e2697d3e2b0fc24bbb56
Kopcap <kopcapx@mail.ru>
2025-12-11 04:01:25 +0500

    sale вниз

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -426,3 +426,22 @@ function getUserOrderSumm($userId)
         return  $totalPaid;
     }
 }
+
+AddEventHandler("search", "BeforeIndex", "BeforeIndexHandler");
+function BeforeIndexHandler($arFields) {
+    /*вместо 000 укажите индетификатор инфоблока каталога*/
+    $arrIblock = array(2);
+    /*исключаем детальное описание и анонс*/
+    $arDelFields = array("DETAIL_TEXT", "PREVIEW_TEXT") ;
+    if (CModule::IncludeModule('iblock') && $arFields["MODULE_ID"] == 'iblock' && in_array($arFields["PARAM2"], $arrIblock) && intval($arFields["ITEM_ID"]) > 0){
+        $dbElement = CIblockElement::GetByID($arFields["ITEM_ID"]) ;
+        if ($arElement = $dbElement->Fetch()){
+            foreach ($arDelFields as $value){
+                if (isset ($arElement[$value]) && strlen($arElement[$value]) > 0){
+                    $arFields["BODY"] = str_replace (CSearch::KillTags($arElement[$value]) , "", CSearch::KillTags($arFields["BODY"]) );
+                }
+            }
+        }
+        return $arFields;
+    }
+}
