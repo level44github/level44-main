a9d845b83aa931b4715391b16975c1076db0a1d0
Kopcap <kopcapx@mail.ru>
2025-12-16 15:00:50 +0500

    Подели

--- local/templates/.default/components/bitrix/sale.order.ajax/main/lang/en/paysystem.php
+++ local/templates/.default/components/bitrix/sale.order.ajax/main/lang/en/paysystem.php
@@ -2,4 +2,5 @@
 $MESS["PAYMENT"] = "3. Payment";
 $MESS["cloudpayments_PAY_SYSTEM_NAME"] = "Payment by bank card / SBP";
 $MESS["dolyame_PAY_SYSTEM_NAME"] = "Dolyame";
+$MESS["podeli_PAY_SYSTEM_NAME"] = "Podeli";
 $MESS["cash_PAY_SYSTEM_NAME"] = "Payment by bank card / cash to the courier";
