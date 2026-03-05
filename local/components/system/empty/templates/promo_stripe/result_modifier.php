15b5aa4e5264b4f6eeddc71571ccc68c5a639b9c
Kopcap <kopcapx@mail.ru>
2026-03-05 14:04:23 +0500

    Плашка сверху ENG

--- local/components/system/empty/templates/promo_stripe/result_modifier.php
+++ local/components/system/empty/templates/promo_stripe/result_modifier.php
@@ -1,5 +1,7 @@
 <?php
 
+$isEn = (defined('SITE_ID') && SITE_ID === 'en');
+
 $res = \CIBlockElement::GetList(
     ['SORT' => 'ASC', 'ID' => 'ASC'],
     [
@@ -12,17 +14,23 @@ $res = \CIBlockElement::GetList(
         'ID',
         'NAME',
         'PROPERTY_LINK_TEXT',
+        'PROPERTY_LINK_TEXT_ENG',
         'PROPERTY_LINK',
         'PROPERTY_COLOR_FON',
         'PROPERTY_COLOR_TEXT',
+        'PROPERTY_TEXT_ENG',
     ]
 );
 
 $arResult['ITEMS'] = [];
 while ($item = $res->GetNext()) {
     $arResult['ITEMS'][] = [
-        'text'       => (string)($item['NAME'] ?? ''),
-        'link_text'  => (string)($item['PROPERTY_LINK_TEXT_VALUE'] ?? ''),
+        'text'       => $isEn
+            ? (string)($item['PROPERTY_TEXT_ENG_VALUE'] ?? $item['NAME'] ?? '')
+            : (string)($item['NAME'] ?? ''),
+        'link_text'  => $isEn
+            ? (string)($item['PROPERTY_LINK_TEXT_ENG_VALUE'] ?? $item['PROPERTY_LINK_TEXT_VALUE'] ?? '')
+            : (string)($item['PROPERTY_LINK_TEXT_VALUE'] ?? ''),
         'link_url'   => (string)($item['PROPERTY_LINK_VALUE'] ?? ''),
         'color_fon'  => (string)($item['PROPERTY_COLOR_FON_VALUE'] ?? '#f5f5f5'),
         'color_text' => (string)($item['PROPERTY_COLOR_TEXT_VALUE'] ?? '#333333'),
