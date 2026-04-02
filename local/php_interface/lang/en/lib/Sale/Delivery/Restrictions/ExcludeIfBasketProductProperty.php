8ef3d8c56e578b189c7cc0099be5e2a84b25b625
Kopcap <kopcapx@mail.ru>
2026-04-03 01:34:07 +0500

    Запрет экспресс-доставки для товара

---
+++ local/php_interface/lang/en/lib/Sale/Delivery/Restrictions/ExcludeIfBasketProductProperty.php
@@ -0,0 +1,5 @@
+<?php
+$MESS['SALE_DELIVERY_RESTRICTION_EXCLUDE_IF_PRODUCT_PROP_NAME'] = 'Hide delivery: NO_EXPRESS = «Yes» (Да)';
+$MESS['SALE_DELIVERY_RESTRICTION_EXCLUDE_IF_PRODUCT_PROP_DESC'] = 'Hides delivery if the basket has a product (or SKU parent) whose NO_EXPRESS list property is set to the enum value titled «Да» (Russian Yes), as configured on the property variant.';
+$MESS['SALE_DELIVERY_RESTRICTION_EXCLUDE_IF_PRODUCT_PROP_ACTIVE'] = 'Enable restriction';
+$MESS['SALE_DELIVERY_RESTRICTION_EXCLUDE_IF_PRODUCT_PROP_CHECK_PARENT'] = 'For SKU, check property on parent product';
---
+++ local/php_interface/lang/ru/lib/Sale/Delivery/Restrictions/ExcludeIfBasketProductProperty.php
@@ -0,0 +1,5 @@
+<?php
+$MESS['SALE_DELIVERY_RESTRICTION_EXCLUDE_IF_PRODUCT_PROP_NAME'] = 'Скрыть доставку: NO_EXPRESS = «Да»';
+$MESS['SALE_DELIVERY_RESTRICTION_EXCLUDE_IF_PRODUCT_PROP_DESC'] = 'Доставка скрывается, если в корзине есть товар (или родитель SKU), у которого свойство NO_EXPRESS (список) выбрано значение с названием «Да» (как у варианта в настройках свойства).';
+$MESS['SALE_DELIVERY_RESTRICTION_EXCLUDE_IF_PRODUCT_PROP_ACTIVE'] = 'Включить ограничение';
+$MESS['SALE_DELIVERY_RESTRICTION_EXCLUDE_IF_PRODUCT_PROP_CHECK_PARENT'] = 'Для SKU проверять свойство у родительского товара';
